8da0dfea64a3c21258042215cf055b05af964b26
Robin Kumar <38507037+robinkumar99@users.noreply.github.com>
2024-06-12 16:20:29 +0530

    initial commit

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -9,7 +9,7 @@ module.exports = createConfig('webpack-dev', {
   },
   resolve: {
     alias: {
-      '@edx/frontend-component-header': path.resolve(__dirname, 'src'),
+      '@skillsenseroot/frontend-component-header': path.resolve(__dirname, 'src'),
     },
   },
 });
